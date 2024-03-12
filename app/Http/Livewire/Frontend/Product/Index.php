48b72770441b0c006fa2223aa07b326c226a0dfd
Rakesh Golani <rakeshgolanieww@gmail.com>
2024-03-12 18:33:28 +0530

    Third Commit

--- app/Http/Livewire/Frontend/Product/Index.php
+++ app/Http/Livewire/Frontend/Product/Index.php
@@ -2,19 +2,42 @@
 
 namespace App\Http\Livewire\Frontend\Product;
 
+use App\Models\Brand;
+use App\Models\Product;
 use Livewire\Component;
 
 class Index extends Component
 {
-    public $products, $category;
+    public $products, $category, $brandInputs = [], $priceInputs;
 
-    public function mount($products, $category)
-    {
-        $this->products = $products;
+    protected $queryString = [
+        'brandInputs' => ['except' => '', 'as' => 'brand'],
+        'priceInputs' => ['except' => '', 'as' => 'price'],
+    ];
+
+    public function mount($category)
+    { 
         $this->category = $category;
     }
+    
     public function render()
     {
+        $this->products = Product::where('category_id',$this->category->id)
+                                    ->when($this->brandInputs, function($q){
+                                        $q->whereIn('brand', $this->brandInputs);
+                                    })
+                                    ->when($this->priceInputs, function($q){
+
+                                        $q->when($this->priceInputs == 'high-to-low', function($q2){
+                                            $q2->orderBy('selling_price','DESC');
+                                        })
+                                        ->when($this->priceInputs == 'low-to-high', function($q2){
+                                            $q2->orderBy('selling_price','ASC');
+                                        });
+                                    })
+                                    ->where('status', '0')
+                                    ->get();
+
         return view('livewire.frontend.product.index', [
             'products' => $this->products,
             'category' => $this->category,
