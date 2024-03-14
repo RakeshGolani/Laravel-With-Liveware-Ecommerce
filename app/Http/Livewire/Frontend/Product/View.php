14e976114b0a41b652890302dbfafd0847bb0328
Rakesh Golani <rakeshgolanieww@gmail.com>
2024-03-14 17:01:35 +0530

    six commit

--- app/Http/Livewire/Frontend/Product/View.php
+++ app/Http/Livewire/Frontend/Product/View.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Livewire\Frontend\Product;
 
+use App\Models\Cart;
 use App\Models\Wishlist;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class View extends Component
 {
-    public $category, $product, $prodColorSelectedQuantity, $quantityCount = 1;
+    public $category, $product, $prodColorSelectedQuantity, $quantityCount = 1, $productColorId;
 
     public function addToWishList($productId)
     {
@@ -55,6 +56,7 @@ class View extends Component
     public function colorSelected($productColorId)
     {
         // dd($productColorId);
+        $this->productColorId = $productColorId;
        $productColor = $this->product->productColors()->where('id',$productColorId)->first();
        $this->prodColorSelectedQuantity = $productColor->colorquantity;
 
@@ -74,6 +76,120 @@ class View extends Component
     {
         if ($this->quantityCount > 1) {
             $this->quantityCount--;
+        }
+    }
+
+    public function addToCart(int $productId)
+    {
+        if (Auth::check()) 
+        {
+            //dd($productId);
+            if ($this->product->where('id', $productId)->where('status', '0')->exists()) 
+            {
+                // Check for product color quantity and add to cart
+                if($this->product->productColors->count() > 1) 
+                {
+                    if($this->prodColorSelectedQuantity != NULL)
+                    {
+                        $productColor = $this->product->productColors->where('id', $this->productColorId)->first();
+                        if ($productColor->colorquantity > 0) {
+                            
+                            if ($productColor->colorquantity > $this->quantityCount) 
+                            {
+                                // Insert Product to Cart
+                                Cart::create([
+                                    'user_id' => auth()->user()->id,
+                                    'product_id' => $productId,
+                                    'product_color_id' => $this->productColorId,
+                                    'quantity' => $this->quantityCount
+                                ]);
+                                $this->dispatchBrowserEvent('message', [
+                                    'text'=> 'Product Added to Cart',
+                                    'type' => 'success',
+                                    'status' => 200
+                                ]);
+                            }
+                            else
+                            {
+                                $this->dispatchBrowserEvent('message', [
+                                    'text'=> 'Only '.$productColor->colorquantity.' Color Quantity Avilable',
+                                    'type' => 'warning',
+                                    'status' => 200
+                                ]);
+                            }
+                        }
+                        else
+                        {
+                            $this->dispatchBrowserEvent('message', [
+                                'text'=> 'Color Out of Stock',
+                                'type' => 'warning',
+                                'status' => 404
+                            ]);
+                        }
+                    }
+                    else
+                    {
+                        $this->dispatchBrowserEvent('message', [
+                            'text'=> 'Select Your Product Color',
+                            'type' => 'info',
+                            'status' => 404
+                        ]);
+                    }
+                }
+                else
+                {
+                    if($this->product->quantity > 0) 
+                    {
+                        if ($this->product->quantity > $this->quantityCount) 
+                        {
+                            // Insert Product to Cart
+                            Cart::create([
+                                'user_id' => auth()->user()->id,
+                                'product_id' => $productId,
+                                'quantity' => $this->quantityCount
+                            ]);
+                            $this->dispatchBrowserEvent('message', [
+                                'text'=> 'Product Added to Cart',
+                                'type' => 'success',
+                                'status' => 200
+                            ]);
+                        }
+                        else
+                        {
+                            $this->dispatchBrowserEvent('message', [
+                                'text'=> 'Only '.$this->product->quantity.' Quantity Avilable',
+                                'type' => 'success',
+                                'status' => 200
+                            ]);
+                        }
+                    }
+                    else
+                    {
+                        $this->dispatchBrowserEvent('message', [
+                            'text'=> 'Out of Stock',
+                            'type' => 'warning',
+                            'status' => 404
+                        ]);
+                    }
+                }
+        
+            }
+            else
+            {
+                $this->dispatchBrowserEvent('message', [
+                    'text'=> 'Product Does not exists',
+                    'type' => 'warning',
+                    'status' => 404
+                ]);
+            }
+        }
+        else
+        {
+            $this->dispatchBrowserEvent('message', [
+                'text'=> 'Please Login to add to cart',
+                'type' => 'info',
+                'status' => 401
+            ]);
         }
     }
 
